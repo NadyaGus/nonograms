398052a1f8d28a7064a78cf689f754a0dd429fcc
NadyaGus <ftotkexit@gmail.com>
2024-06-25 16:15:57 +0300

    feat: add base button

---
+++ src/utils/baseButton.ts
@@ -0,0 +1,16 @@
+import BaseComponent from './baseComponent';
+
+import classes from './button.module.scss';
+
+type Props = {
+  text: string;
+  onClick: () => void;
+};
+
+export class BaseButton extends BaseComponent {
+  constructor({ text, onClick }: Props) {
+    super({ tag: 'button', className: classes.button });
+    this.setTextContent(text);
+    this.addListener('click', onClick);
+  }
+}
